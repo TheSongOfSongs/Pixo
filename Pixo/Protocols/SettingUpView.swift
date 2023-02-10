73bc8b0820d53d0a137f0cea6ee7df1245440eb0
TheSongOfSongs <21500171@handong.edu>
2023-02-10 16:16:08 +0900

    Feat: protocol SettingUpView 구현

---
+++ Pixo/Protocols/SettingUpView.swift
@@ -0,0 +1,23 @@
+//
+//  SettingUpView.swift
+//  Pixo
+//
+//  Created by Jinhyang Kim on 2023/02/10.
+//
+
+import UIKit
+
+protocol SettingUpView {
+    func setupLayout()
+    /// UIView Element를 추가해주는 함수
+    func setupView()
+    /// UIView Element의 AutoLayout을 잡아주는 함수
+    func setupConstriants()
+}
+
+extension SettingUpView {
+    func setupLayout() {
+        setupView()
+        setupConstriants()
+    }
+}
