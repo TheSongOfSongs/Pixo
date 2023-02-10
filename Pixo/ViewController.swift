8e5362853e70eab3d86e63f954b4e07f92f57ad0
TheSongOfSongs <21500171@handong.edu>
2023-02-10 13:05:09 +0900

    first commit

---
+++ Pixo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pixo
+//
+//  Created by Jinhyang Kim on 2023/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
