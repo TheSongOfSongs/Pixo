c30654ee7053c29d345c16f1b5a224efe32c5ca8
TheSongOfSongs <21500171@handong.edu>
2023-02-11 18:30:38 +0900

    Feat: PhotosManager - 앨범 리스트를 가져와 Album형태로 전달하는 기능 구현

---
+++ Pixo/Models/Album.swift
@@ -0,0 +1,16 @@
+//
+//  Album.swift
+//  Pixo
+//
+//  Created by Jinhyang Kim on 2023/02/11.
+//
+
+import Foundation
+import Photos
+
+struct Album {
+    var type: AlbumSection
+    var phAssetCollection: PHAssetCollection?
+    var phFetchResult: PHFetchResult<PHAsset>
+    var title: String
+}
---
+++ Pixo/Utils/PhotosManager.swift
@@ -0,0 +1,79 @@
+//
+//  PhotosManager.swift
+//  Pixo
+//
+//  Created by Jinhyang Kim on 2023/02/10.
+//
+
+import UIKit
+import Photos
+
+final class PhotosManager {
+    
+    typealias Albums = (allPhotos: PHFetchResult<PHAsset>,
+                        smartAlbums: PHFetchResult<PHAssetCollection>,
+                        userCollections: PHFetchResult<PHCollection>)
+    
+    // 최신순으로 사진만 가져오는 옵션
+    let imageFetchingOptions = PHFetchOptions().then {
+        $0.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        $0.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
+    }
+    
+    // MARK: -
+    func fetchAllPhotosAlbum() -> Album {
+        let allPhotos = PHAsset.fetchAssets(with: imageFetchingOptions)
+        return Album(type: .allPhotos,
+                     phFetchResult: allPhotos,
+                     title: "All Photos")
+    }
+    
+    func fetchSmartAlbums() -> [Album] {
+        let fetchingResult = PHAssetCollection.fetchAssetCollections(with: .smartAlbum,
+                                                                     subtype: .albumRegular,
+                                                                     options: nil)
+        
+        var smartAlbums: [Album] = []
+        fetchingResult.enumerateObjects { collection, index, _ in
+            let phFetchResult = PHAsset.fetchAssets(in: collection, options: self.imageFetchingOptions)
+            
+            if phFetchResult.count == 0 {
+                return
+            }
+
+            smartAlbums.append(
+                Album(type: .smartAlbums,
+                      phAssetCollection: collection,
+                      phFetchResult: phFetchResult,
+                      title: collection.localizedTitle ?? "")
+            )
+        }
+        
+        return smartAlbums
+    }
+    
+    func fetchUserCollectionAlbums() -> [Album] {
+        let fetchingResult = PHAssetCollection.fetchAssetCollections(with: .album,
+                                                                     subtype: .albumRegular,
+                                                                     options: nil)
+        
+        var userCollections: [Album] = []
+        fetchingResult.enumerateObjects { collection, index, _ in
+            let phFetchResult = PHAsset.fetchAssets(in: collection, options: self.imageFetchingOptions)
+            
+            if phFetchResult.count == 0 {
+                return
+            }
+
+            userCollections.append(
+                Album(type: .smartAlbums,
+                      phAssetCollection: collection,
+                      phFetchResult: phFetchResult,
+                      title: collection.localizedTitle ?? "")
+            )
+        }
+        
+        return userCollections
+    }
+}
+
