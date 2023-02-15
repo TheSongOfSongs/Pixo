3bf75eddf51a09b979797e68e7d8812df70588fc
TheSongOfSongs <21500171@handong.edu>
2023-02-15 16:36:25 +0900

    Feat: FirebaseStorage에서 svg 이미지 가져오는 클래스 구현

---
+++ Pixo/Utils/SVGImageManager.swift
@@ -0,0 +1,43 @@
+//
+//  SVGImageManager.swift
+//  Pixo
+//
+//  Created by Jinhyang Kim on 2023/02/15.
+//
+
+import Foundation
+import FirebaseStorage
+
+
+/// FirebaseStorage로부터 svg 이미지를 가져오는 기능을 담당합니다
+final class SVGImageManager {
+    
+    /// 한 번 요청 시 최대로 가져올 수 있는 이미지 수
+    let maxResults: Int64 = 20
+    
+     /// API를 통해 반환했을 때 이전 결과에서 반환된 마지막 항목의 경로와 버전을 인코딩한 값
+    var pageToken: String?
+    
+    /**
+     저장소에서 이미지를 가져옵니다.
+     pageToken 값이 존재하면 후속요청으로 pageToken 뒤에 오는 항목들이 반환됩니다.
+     */
+    func fetchSVGImages() async -> Result<[StorageReference], Error> {
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        do {
+            var result: StorageListResult
+            if let pageToken = pageToken {
+                result = try await storageReference.list(maxResults: maxResults, pageToken: pageToken)
+            } else {
+                result = try await storageReference.list(maxResults: maxResults)
+            }
+            
+            return .success(result.items)
+        } catch let error {
+            NSLog("❗️ error ==> \(error.localizedDescription)")
+            return .failure(error)
+        }
+    }
+}
